3276188cf81fd4251c78aeed300901439cda34a7
anisskiller <anissaallou36@gmail.com>
2018-11-16 14:44:55 +0100

    Update AdressePostale.java

--- src/entites/AdressePostale.java
+++ src/entites/AdressePostale.java
@@ -6,7 +6,11 @@ public class AdressePostale {
 	public String libelleVoie = "Times Square Gardens";
 	public int codePostal = 10010;
 	public String ville = "NY City";
+	
+	// instanciation de classe
+	AdressePostale monAdressePostale = new AdressePostale(18, "Baker Street", 10001, "NY");
 
+	//constructeur 
 	public AdressePostale(int numeroRue, String libelleVoie, int codePostal, String ville) {
 		this.numeroRue = numeroRue;
 		this.libelleVoie = libelleVoie;
