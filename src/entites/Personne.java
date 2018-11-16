fc8f7114c6ae74a5ab86b9d0e5b2b407eae6f644
anisskiller <anissaallou36@gmail.com>
2018-11-16 14:54:37 +0100

    Update Personne.java

--- src/entites/Personne.java
+++ src/entites/Personne.java
@@ -11,13 +11,14 @@ public class Personne {
 
 	
 	
-	
+	// Constructeur valorisant les attributs suivants : nom, prenom.
 	public Personne(String nom, String prenom) {
 		this.nom = nom; 
 		this.prenom = prenom;
 		
 	}
 	
+	// Constructeur valorisant les attributs suivants : nom, prenom, adressePostale
 	public Personne(String nom, String prenom, String adressePostale) {
 		this.nom = nom; 
 		this.prenom = prenom;
@@ -31,37 +32,47 @@ public class Personne {
 			
 		}	
 	
+	// setter ("mutateur") pour l'attribut nom
 	public void setNom(String nom) {	
 		this.nom = nom;
 	}
 	
+	// setter ("mutateur") pour l'attribut prenom
 	public void setPrenom(String prenom) {	
 		this.prenom = prenom;
 	}
 	
+	// setter ("mutateur") pour l'attribut adressePostale
 	public void setAdresse(String adressePostale) {	
 		this.adressePostale = adressePostale;
 	}
 	
+	// getter ("accesseur") pour l'attribut nom
 	public String getNom() {
 		return nom;
 	}
 	
+	// getter ("accesseur") pour l'attribut prenom
 	public String getPrenom() {
 		return prenom;
 	}
 	
+	// getter ("accesseur") pour l'attribut adressePostale
 	public String getAdresse() {
 		return adressePostale;
 	}
 	
+	// getter ("accesseur") pour l'attribut anneeNaissance
 	public int getAnneeNaissance() {
 		return anneeNaissance;
 	}
+
+	// setter ("mutateur") pour l'attribut anneeNaissance
 	public void setAnneeNaissance(int anneeNaissance) {
 		this.anneeNaissance = anneeNaissance;
 	}
 	
+	// méthode qui retourne l'âge de la personne
 	public int getAge() {
 		Calendar c = Calendar.getInstance();
 		int year = c.get(Calendar.YEAR);
