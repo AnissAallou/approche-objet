cc8f69fc62d80373b72fd1737f4dd1ca53dd5bc7
anisskiller <anissaallou36@gmail.com>
2018-11-19 00:50:52 +0100

    Update TestPersonne.java

--- src/essais/TestPersonne.java
+++ src/essais/TestPersonne.java
@@ -1,48 +1,17 @@
 package essais;
-
 import entites.Personne;
-import entites.AdressePostale;
 
 public class TestPersonne {
+
 	public static void main(String[] args) {
 		
-		Personne personne = new Personne("Aallou", "Aniss");
-		personne.nom = "Aallou";
-		personne.prenom = "Aniss";
-		personne.adressePostale = "mdr";
+		// 2 instances de la classe Personne
+		Personne personne1 = new Personne("Aallou","Aniss","42 rue Mont Saint Michel",1995);
+		Personne personne2 = new Personne("Cabella", "Rémy","32 rue des Haricots", 1988);
 		
-		Personne personne2 = new Personne("Wick", "Michael");
-		personne2.nom = "Wick";
-		personne2.prenom = "Michael";
-		personne2.adressePostale = "lol";
-		
-		Personne personne3 = new Personne("Wayne", "John");
-		personne3.nom = "Wayne";
-		personne3.prenom = "John";
-		
-		Personne personne4 = new Personne("Pirlo", "Andrea");
-		personne4.nom = "Pirlo";
-		personne4.prenom = "Andrea";
-		personne4.adressePostale = "Milano";
-		
-		
-		// Modifier la classe TestPersonne de manière à utiliser le premier constructeur de la
-		//classe Personne pour valoriser les attributs nom et prenom ainsi que la méthode
-		// setAdresse() qui permet de modifier l’adresse pour valoriser l’adresse.
-		
-		Personne personne5 = new Personne("Giggs", "Ryan");
-		personne5.nom = "Giggs";
-		personne5.prenom = "Ryan";
-		
-		
-		
+		personne1.printPersonne();
+		personne2.printPersonne();
+		System.out.println(personne1.getAge());
 	}
-	public void setAdresse(String adressePostale) {
-		this.monAdressePostale = adressePostale;
-	}
-	}
-	
-	
-	
-	
 
+}
