ce3bf2e17156c064b2078b019867a7e971a64a92
anisskiller <anissaallou36@gmail.com>
2018-11-19 00:48:21 +0100

    Update AdressePostale.java

--- src/entites/AdressePostale.java
+++ src/entites/AdressePostale.java
@@ -7,8 +7,7 @@ public class AdressePostale {
 	public int codePostal = 10010;
 	public String ville = "NY City";
 	
-	// instanciation de classe
-	AdressePostale monAdressePostale = new AdressePostale(18, "Baker Street", 10001, "NY");
+	
 
 	//constructeur 
 	public AdressePostale(int numeroRue, String libelleVoie, int codePostal, String ville) {
@@ -17,4 +16,52 @@ public class AdressePostale {
 		this.codePostal = codePostal;
 		this.ville = ville;
 	}
+	
+	// getters (accesseurs)
+	
+	public AdressePostale(String libelleVoie, String ville, int codePostal) {
+		this.libelleVoie = libelleVoie; 
+		this.ville = ville; 
+		this.codePostal = codePostal;
+	}
+
+	public int getNumeroRue() {
+		return numeroRue;
+	}
+
+	public String getLibelleVoie() {
+		return libelleVoie;
+	}
+	
+	public String getVille() {
+		return ville;
+	}
+	
+	public int getCodePostal() {
+		return codePostal;
+	}
+	
+	
+	// setters (mutateurs)
+	
+	public void setNumeroRue(int numeroRue) {
+		this.numeroRue = numeroRue;
+	}
+
+	public void setLibelleVoie(String libelleVoie) {
+		this.libelleVoie = libelleVoie;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public void setCodePostal(int codePostal) {
+		this.codePostal = codePostal;
+	}
+	
+	public void printAdresse() { // méthode pour afficher l'adresse
+		System.out.println("numéro de Rue => " + this.numeroRue + " libellé de voie => " + this.libelleVoie + " ville => " + this.ville + " code postal => " + this.codePostal);
+	}
+
 }
