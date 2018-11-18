97edf8370da255292fceadfb80fc626796cc2a4c
anisskiller <anissaallou36@gmail.com>
2018-11-19 00:50:13 +0100

    Update TestAdressePostale.java

--- src/essais/TestAdressePostale.java
+++ src/essais/TestAdressePostale.java
@@ -7,9 +7,14 @@ import entites.AdressePostale;
 public class TestAdressePostale {
 	public static void main(String[] args) {
 		
-		AdressePostale adressePostale = new AdressePostale(18, "Times Square Gardens", 10010, "NY City");
-		AdressePostale monAdressePostale = new AdressePostale(18, "Baker Street", 10001, "NY");
+		// instanciation
+		AdressePostale adressePostale1 = new AdressePostale("Times Square Gardens", "NY City", 10010);
 		
+		adressePostale1.printAdresse();
+				
+				
+				
+				
 	
 	}
 	
