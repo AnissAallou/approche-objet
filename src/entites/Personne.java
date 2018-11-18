c5060f16ab34731a20310d4b81543b3e73a8dfcf
anisskiller <anissaallou36@gmail.com>
2018-11-19 00:49:23 +0100

    Update Personne.java

--- src/entites/Personne.java
+++ src/entites/Personne.java
@@ -1,50 +1,35 @@
 package entites;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class Personne {
 
 	public String nom; 
 	public String prenom;
-	public String adressePostale; 
+	public String adressePostale;
 	public int anneeNaissance;
 
-	
-	
-	// Constructeur valorisant les attributs suivants : nom, prenom.
-	public Personne(String nom, String prenom) {
-		this.nom = nom; 
-		this.prenom = prenom;
-		
-	}
+
 	
 	// Constructeur valorisant les attributs suivants : nom, prenom, adressePostale
-	public Personne(String nom, String prenom, String adressePostale) {
+	public Personne(String nom, String prenom, String adressePostale, int anneeNaissance) {
+		
 		this.nom = nom; 
 		this.prenom = prenom;
 		this.adressePostale = adressePostale; 
+		this.anneeNaissance = anneeNaissance;
 		
 	}
 	
-	public String toString(String nom, String prenom) {
-	
-			return "nom => " + nom + ", prenom => " + prenom;
-			
-		}	
-	
-	// setter ("mutateur") pour l'attribut nom
-	public void setNom(String nom) {	
-		this.nom = nom;
+	public void printPersonne() { // méthode pour afficher le nom, le prénom et l'adresse
+		System.out.println("Nom => " + this.nom + "; prenom => " + this.prenom + "; adresse => " + this.adressePostale);
 	}
+
 	
-	// setter ("mutateur") pour l'attribut prenom
-	public void setPrenom(String prenom) {	
-		this.prenom = prenom;
-	}
-	
-	// setter ("mutateur") pour l'attribut adressePostale
-	public void setAdresse(String adressePostale) {	
-		this.adressePostale = adressePostale;
+	// méthode qui retourne l'âge de la personne
+	public String getAge() {
+		int year = Calendar.getInstance().get(Calendar.YEAR); // renvoie la valeur du champ de calendrier donné.
+		return year - this.anneeNaissance + " ans";
 	}
 	
 	// getter ("accesseur") pour l'attribut nom
@@ -66,20 +51,30 @@ public class Personne {
 	public int getAnneeNaissance() {
 		return anneeNaissance;
 	}
+	
+	// setter ("mutateur") pour l'attribut nom
+	public void setNom(String nom) {	
+		this.nom = nom;
+	}
+	
+	// setter ("mutateur") pour l'attribut prenom
+	public void setPrenom(String prenom) {	
+		this.prenom = prenom;
+	}
+	
+	// setter ("mutateur") pour l'attribut adressePostale
+	public void setAdresse(String adressePostale) {	
+		this.adressePostale = adressePostale;
+	}
 
 	// setter ("mutateur") pour l'attribut anneeNaissance
 	public void setAnneeNaissance(int anneeNaissance) {
 		this.anneeNaissance = anneeNaissance;
 	}
 	
-	// méthode qui retourne l'âge de la personne
-	public int getAge() {
-		Calendar c = Calendar.getInstance();
-		int year = c.get(Calendar.YEAR);
-		return year = this.anneeNaissance;
-	}
+
 	
-	}
+}
 
 
 
